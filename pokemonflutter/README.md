4289f56b01526b84d3121b4b17c8c9ade0cca461
Robin Mahieu <mahieurobindev>
2021-11-10 12:04:50 +0100

    change name

--- pokemonflutter/README.md
+++
@@ -1,6 +0,0 @@
-# projet-pokemon
-DEV A -> Valentin HIPPOLYTE
-
-DEV B -> Robin MAHIEU
-
-DEV C -> Tony PAQUES
--- pokemonflutter/android/app/src/main/kotlin/com/example/pokemonflutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pokemonflutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
