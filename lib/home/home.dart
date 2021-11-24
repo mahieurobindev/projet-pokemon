5b60d5b86b3bf7b2e3d699b465f922cc3e2316ee
Robin Mahieu <mahieurobindev>
2021-11-24 12:45:45 +0100

    ajout du bouton incrementable dans home

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Image.asset('images/pokemon.png'),
+    return Scaffold(
+      body: Center(child: Image.asset('images/pokemon.png')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Increment Counter',
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
