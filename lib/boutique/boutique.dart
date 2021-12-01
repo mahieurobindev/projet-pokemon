9dd5c3710c3afe0d2f0450e350ff8ec2e5e94135
valentin.hippolyte <valenthippo@gmail.com>
2021-12-01 12:34:49 +0100

    requete api by id dans boutique

--- lib/boutique/boutique.dart
+++ lib/boutique/boutique.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokemonflutter/models/pokemon.dart';
 import 'package:pokemonflutter/providers/dio.dart';
 
 class Boutique extends ConsumerWidget {
@@ -10,13 +13,15 @@ class Boutique extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
 
     final Dio dio =ref.read(dioProvider);
-    final Future<Response> future = dio.get("https://pokeapi.co/api/v2/");
+    final Future<Response> future = dio.get("/pokemon/10");
 
-    future.then((Response value) =>{
-      print(value.statusCode)
+    future.then((Response value){
+      print (value.toString());
+      GetPokemonByIdResponse pokemon  = GetPokemonByIdResponse.fromJson(jsonDecode(value.toString()));
+      print(pokemon.name);
     })
     .catchError((onError){
-      print("impossible re récupérer le pokemon");
+      print("impossible de récupérer le pokemon");
     });
     return Container(
       child: const Text('Boutique'),
