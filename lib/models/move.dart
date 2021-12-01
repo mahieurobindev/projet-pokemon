b7e06d030b867098285f42c70f731486c37dfc40
valentin.hippolyte <valenthippo@gmail.com>
2021-12-01 09:45:08 +0100

    ajout des classes Pokemon et Move avec les dépendences

---
+++ lib/models/move.dart
@@ -0,0 +1,22 @@
+
+
+import 'package:json_annotation/json_annotation.dart';
+import 'package:pokemonflutter/models/pokemon.dart';
+
+part 'move.g.dart';
+
+@JsonSerializable()
+class Move{
+final String? name;
+final String? url;
+
+Move(this.name, this.url);
+
+
+factory Move.fromJson(Map<String,dynamic> json) =>_$MoveFromJson(json);
+
+
+
+Map<String, dynamic> toJson() => _$MoveToJson(this);
+
+}
